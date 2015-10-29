dfe88a9c37c25ae27bde7841bd0bd7ef4ccbc682
Nicholis Wood <nicholis430@gmail.com>
2015-10-28 19:07:25 -0700

    Initial Commit

---
+++ nwood-utils/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nwood-utils
+//
+//  Created by Nicholis Wood on 10/28/15.
+//  Copyright © 2015 Nicholis Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
